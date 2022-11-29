48deb636b5cd96e8cc43416ba532e24fb5872b80
Ygor Bravim <ygorbravimr@gmail.com>
2022-11-29 07:55:20 -0300

    mod

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,6 @@
-export const priceFormatter = new Intl.NumberFormat('pt-Br', {
-  style: 'currency',
-  currency: 'BRL',
-})
+export const priceFormatter = new Intl.NumberFormat("pt-Br", {
+  style: "currency",
+  currency: "BRL",
+});
 
-export const dateFormatter = new Intl.DateTimeFormat('pt-Br')
+export const dateFormatter = new Intl.DateTimeFormat("pt-Br");
